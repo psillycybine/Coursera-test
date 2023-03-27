1e153ee10a6f891b5cce771155cfd7b21422c6ee
rnettlow <yahoo4198@gmail.com>
2023-03-26 19:30:44 -0800

    mod 4 with update

--- mod4_solution/SpeakHello.js
+++ mod4_solution/SpeakHello.js
@@ -6,7 +6,6 @@
   // the "speak" method and which you will expose to the global context
   // See Lecture 52, part 1
   var helloSpeaker = {};
-
   // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
   var speakWord = "Hello";
   // STEP 4: Rewrite the 'speak' function such that it is attached to the
